0e909602aba86b545b6b1249353d1f347abf7759
Ali Ibrahim <alialchi07@gmail.com>
2023-07-19 14:47:34 -0500

    Testing Launch

--- pages/items/PeopleToFollow.tsx
+++ pages/items/PeopleToFollow.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
-import Image from "./Image";
+import Images from "./Images";
 
 const PeopleToFollow = () => {
   const [post, setPost] = useState("");
@@ -11,11 +11,11 @@ const PeopleToFollow = () => {
       <div className="flex flex-col bg-slate-200 gap-10 p-7">
         <h1 className="text-2xl">Consider following</h1>
         <div style={{ height: "270px" }} className="flex gap-3 overflow-auto">
-          <Image imageUrl="./file.jpg" imageName="file" type="rectangle" />
-          <Image imageUrl="./logo.png" imageName="file" type="rectangle" />
-          <Image imageUrl="./tech.jpg" imageName="file" type="rectangle" />
-          <Image imageUrl="./tech.jpg" imageName="file" type="rectangle" />
-          <Image imageUrl="./tech.jpg" imageName="file" type="rectangle" />
+          <Images imageUrl="/file.jpg" imageName="file" type="rectangle" />
+          <Images imageUrl="/logo.png" imageName="file" type="rectangle" />
+          <Images imageUrl="/" imageName="file" type="rectangle" />
+          <Images imageUrl="/tech.jpg" imageName="file" type="rectangle" />
+          <Images imageUrl="/tech.jpg" imageName="file" type="rectangle" />
         </div>
       </div>
     </div>
